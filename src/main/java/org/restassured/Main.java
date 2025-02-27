6ea3cf9237b6a839098da555315b695c3b4704b5
Manoj Thiyagarajan <manojt2409@gmail.com>
2025-02-28 00:13:32 +0530

    extracted value from response body using json path

--- src/main/java/org/restassured/Main.java
+++ src/main/java/org/restassured/Main.java
@@ -1,6 +1,8 @@
 package org.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -8,8 +10,17 @@ public class Main {
     public static void main(String[] args) {
     //setting the base uri
         RestAssured.baseURI="https://rahulshettyacademy.com";
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+        String response=given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
                 .body(payload.getBody())
-                .when().post("maps/api/place/add/json").then().log().all().statusCode(200).assertThat().body("scope",equalTo("APP")).header("Server",equalTo("Apache/2.4.52 (Ubuntu)"));
+        .when().post("maps/api/place/add/json").then().log().all().statusCode(200).assertThat().body("scope",equalTo("APP")).header("Server",equalTo("Apache/2.4.52 (Ubuntu)"))
+                .extract().response().asString();
+        System.out.println("Response : "+response);
+
+        //extract the place_id from the response
+        JsonPath jsonPath=new JsonPath(response);
+        String place_id=jsonPath.getString("place_id");
+        System.out.println("Place id : "+place_id);
+
+
     }
 }
